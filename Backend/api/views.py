b3d94028825a5e1c5275e0545402664f716a8709
kimchimanz <65414004+kimchimanz@users.noreply.github.com>
2026-01-20 23:24:26 +0900

    as

--- Backend/api/views.py
+++ Backend/api/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# 요청이 오면 "Hello World"를 응답하는 함수
+def hello_world(request):
+    return HttpResponse("Hello World")
